28a826ee39e47a80bdf04191fb9337205a14808b
Ana Reis <86137114+dorazzi@users.noreply.github.com>
2022-05-04 21:00:34 +0000

    Orm/2

--- index.js
+++ index.js
@@ -12,9 +12,28 @@ app.get("/", async function(req, res){
   res.json(mostrar);
 });
 
+app.get("/:id", async function(req,res){
+  var resultado = await usuario.findAll();
+  res.json(resultado);
+});
+
+
 app.post("/", function(req, res){
   var resultado = usuario.create(req.body);
   res.json(resultado);
+});
+
+app.put("/:id", async function(req, res){
+var atualizar = await usuario.findByPk(req.params.id);
+  res.send(atualizar.nome = "Ana");
+
+  var resultadoSave = await atualizar.save();
+  console.log(resultadoSave);
+});
+
+app.delete("/:id", async function (req,res){
+  const deletar = await usuario.findByPk(req.params.id);
+  deletar.destroy();
 });
 
 app.listen(3000, function(){
